70b775247cc21f52c9bc628202db30b8ad78a8a3
Hans Markwart <hans@rapidrabbit.com>
2024-04-03 14:07:55 +0200

    - removed some unused stuff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,6 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    plugins {
-        kotlin("jvm").version(extra["kotlin.version"] as String)
-        id("org.jetbrains.compose").version(extra["compose.version"] as String)
-        id ("org.jetbrains.kotlin.plugin.serialization").version(extra["kotlin.version"] as String)
-    }
 }
 
 rootProject.name = "NextCloudPhotoRenamer"
